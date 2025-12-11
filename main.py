d0069197684a077194b7155ab5cf384789344d40
Mbote Ivy <mboteivy@gmail.com>
2025-12-11 17:19:28 +0300

    program to add number

---
+++ main.py
@@ -0,0 +1,15 @@
+'''
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+'''
+num1=int(input("Enter the first number"))
+num2=int(input("Enter the second number"))
+num3=int(input("Enyer the third number"))
+sum=num1+num2+num3
+print("the sum is",sum)
+
+
